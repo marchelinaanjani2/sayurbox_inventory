a74c6e2d52e7ab78c722ecc7272efb10a7c2e631
Marchelina Anjani S <marselinaanjanisaputri23@gmail.com>
2023-09-26 19:54:41 +0700

    tugas 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
@@ -10,7 +11,8 @@ class Item(models.Model):
     stock = models.IntegerField(default=0)  
 
     image = models.ImageField(upload_to='product_images/', default='default_image.jpg') 
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
         return self.name
     
--- main/views.py
+++ main/views.py
@@ -7,29 +7,42 @@ from main.models import Item
 from django.http import HttpResponse
 from django.core import serializers
 from django.db.models import Sum
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages  
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+
+@login_required(login_url='/login')
 # Create your views here.
 def show_main(request):
-    products = Item.objects.all()
+    products = Item.objects.filter(user=request.user)
     total_stock = products.aggregate(total_stock=Sum('amount'))['total_stock'] or 0
     context = {
         'products': products,
-        'name': 'Marchelina Anjani S', 
+        'nama ' : 'Marchelina Anjani S',
         'class': 'PBP B',
+        'name': request.user.username, 
         'total_stock': total_stock, 
+        'last_login': request.COOKIES['last_login'],
      }
         
     return render(request, "main.html", context)
 
 
 def create_product(request):
-    if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.stock = product.amount  # Set stok awal sama dengan jumlah produk yang ditambahkan
-            product.save()
-            return HttpResponseRedirect(reverse('main:show_main'))
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        product = form.save(commit=False)
+        product.user = request.user
+        product.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
     else:
         form = ProductForm()
 
@@ -52,3 +65,39 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Item.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+
